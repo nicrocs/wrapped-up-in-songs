a377c76170b4b800828539d87691344c5c8a92a8
Nic Guest-Jelley <nicgjelley@Nics-MacBook-Pro-2.local>
2026-03-30 14:44:41 -0700

    fix captilization issue

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,7 @@ import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
 import IconFacebook from "@/assets/icons/IconFacebook.svg";
 import IconTelegram from "@/assets/icons/IconTelegram.svg";
 import IconPinterest from "@/assets/icons/IconPinterest.svg";
-import IconBluesky from '@/assets/icons/iconBluesky.svg';
+import IconBluesky from '@/assets/icons/IconBluesky.svg';
 import { SITE } from "@/config";
 
 interface Social {
