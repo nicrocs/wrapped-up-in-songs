4fa70736b89278ef98c14bd28c92a72918649e36
Nic Guest-Jelley <nicgjelley@Nics-MacBook-Pro-2.local>
2026-03-30 14:26:43 -0700

    add bluesky and remove boilerplate

--- src/constants.ts
+++ src/constants.ts
@@ -2,11 +2,11 @@ import type { Props } from "astro";
 import IconMail from "@/assets/icons/IconMail.svg";
 import IconGitHub from "@/assets/icons/IconGitHub.svg";
 import IconBrandX from "@/assets/icons/IconBrandX.svg";
-import IconLinkedin from "@/assets/icons/IconLinkedin.svg";
 import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
 import IconFacebook from "@/assets/icons/IconFacebook.svg";
 import IconTelegram from "@/assets/icons/IconTelegram.svg";
 import IconPinterest from "@/assets/icons/IconPinterest.svg";
+import IconBluesky from '@/assets/icons/iconBluesky.svg';
 import { SITE } from "@/config";
 
 interface Social {
@@ -19,25 +19,19 @@ interface Social {
 export const SOCIALS: Social[] = [
   {
     name: "GitHub",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/nicrocs/wrapped-up-in-songs",
     linkTitle: `${SITE.title} on GitHub`,
     icon: IconGitHub,
   },
   {
-    name: "X",
-    href: "https://x.com/username",
-    linkTitle: `${SITE.title} on X`,
-    icon: IconBrandX,
-  },
-  {
-    name: "LinkedIn",
-    href: "https://www.linkedin.com/in/username/",
-    linkTitle: `${SITE.title} on LinkedIn`,
-    icon: IconLinkedin,
+    name: "BlueSky",
+    href: "https://bsky.app/profile/nicguestjelley.bsky.social",
+    linkTitle: "Me on BlueSky",
+    icon: IconBluesky,
   },
   {
     name: "Mail",
-    href: "mailto:yourmail@gmail.com",
+    href: "mailto:ngjelley@gmail.com",
     linkTitle: `Send an email to ${SITE.title}`,
     icon: IconMail,
   },
@@ -57,10 +51,10 @@ export const SHARE_LINKS: Social[] = [
     icon: IconFacebook,
   },
   {
-    name: "X",
-    href: "https://x.com/intent/post?url=",
-    linkTitle: `Share this post on X`,
-    icon: IconBrandX,
+    name: "BlueSky",
+    href: "https://bsky.app/intent/compose?text=",
+    linkTitle: "Share this post on BlueSky",
+    icon: IconBluesky
   },
   {
     name: "Telegram",
